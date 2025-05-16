6498a7672bc8467987630e660091a335bc19f518
SamCodeX01 <txtstring@gmail.com>
2025-05-16 17:01:23 -0300

    Add arquivo

---
+++ src/CadastroDeClientes_02.java
@@ -0,0 +1,6 @@
+/*2. Cadastro de clientes
+Crie um sistema que permita cadastrar N clientes, solicitando nome e telefone, usando for para repetir N vezes.
+*/
+public class CadastroDeClientes_02 {
+
+}
