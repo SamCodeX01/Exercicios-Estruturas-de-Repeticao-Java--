9f43d7c16fd7e7db43fbcf564fb8199d59be5185
SamCodeX01 <txtstring@gmail.com>
2025-05-17 00:33:32 -0300

    Exercicio 2 atualizado

--- src/CadastroDeClientes_02.java
+++ src/CadastroDeClientes_02.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 /*2. Cadastro de clientes
-Crie um sistema que permita cadastrar N clientes, solicitando nome e telefone, usando for para repetir N vezes.
+Crie um sistema que permita cadastrar N clientes,
+solicitando nome e telefone, usando for para repetir N vezes.
 */
 public class CadastroDeClientes_02 {
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        String cliente, telefone,simOuNao;
+
+        do{
+            System.out.print("Cliente: ");
+            cliente = sc.next();
+
+            System.out.print("Telefone: ");
+            telefone = sc.next();
+
+            System.out.println("Digite S para continuar ou N para Sair ");
+            simOuNao = sc.next();
+        }
+        while(simOuNao.equalsIgnoreCase("S"));
+    }
 }
