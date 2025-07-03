bd8d4e108be54f81847c2875c0d45a27927294f8
SamCodeX01 <txtstring@gmail.com>
2025-07-02 23:30:49 -0300

    Exercicio 3, feito!

--- src/Ex03_ContadorDeLogin.java
+++ src/Ex03_ContadorDeLogin.java
@@ -2,21 +2,37 @@ import java.util.Scanner;
 
 public class Ex03_ContadorDeLogin {
     public static void main(String[] args) {
-//Importe vários usuários e conte quantos tentaram login,
-// usando while para registrar tentativas até atingir um limite (ex: 3 tentativas).
+/*Importe vários usuários e conte quantos tentaram login,
+ usando while para registrar tentativas até atingir um limite (ex: 3 tentativas).*/
         Scanner sc = new Scanner(System.in);
-//        String login, senha;
-        int tentativas = 3;
 
-/*
-        while(tentativas <3){
-            System.out.println("Login: ");
-            String login = sc.next();
-            System.out.println("Senha: ");
-            String senha = sc.next();
-            if(login.equalsIgnoreCase("sam" && senha.equals("123"))){}
-            System.out.println();
+        String loginCorreto = "sa";
+        String loginDigitado;
+        String senhaCorreta = "123";
+        String senhaDigitada;
+        int tentativas = 0, maxTentativas = 3;
+        boolean logado = false;
+
+        //while (!loginDigitado.equals(loginCorreto) || !senhaDigitada.equals(senhaCorreta)) {
+        while (tentativas < maxTentativas) {
+                    System.out.print("Login: ");
+                    loginDigitado = sc.next();
+                    System.out.print("Senha: ");
+                    senhaDigitada = sc.next();
+
+                if (loginDigitado.equals(loginCorreto) && senhaDigitada.equals(senhaCorreta)) {
+                    System.out.println("Login Efetuado com sucesso!");
+                    logado = true;
+                }
+                 else{
+                   tentativas++;
+                   System.out.println("Login ou senha incorretos!");
+                   System.out.println("Numero de tentativas: " + tentativas);
+                   System.out.println("Numero de tentativas restantes: " + (maxTentativas - tentativas));
+                 }
+            }
+                 if (!logado){
+                   System.out.println("Numero de tentativas excedidas!");
+                 }
         }
-*/
-    }
 }
