a72e0376faf766244cbccdf2aec4d5820502f2ef
SamCodeX01 <txtstring@gmail.com>
2025-07-03 00:32:19 -0300

    Exercicio 4, feito!

--- src/Ex04_Lojas.java
+++ src/Ex04_Lojas.java
@@ -7,32 +7,23 @@ de vários produtos até decidir parar
 */
 public class Ex04_Lojas {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        Double p1, p2, p3, p4;
-        String x;
+        Scanner sc = new Scanner(System.in);
+        int contador = 0;
+        double preco, total = 0;
+        String situacao;
 
-        do {
-        System.out.println("Insira o preço dos produtos: ");
-
-            System.out.print("Produto 1: ");
-            p1 = sc.nextDouble();
-            System.out.print("Produto 2: ");
-            p2 = sc.nextDouble();
-            System.out.print("Produto 3: ");
-            p3 = sc.nextDouble();
-            System.out.print("Produto 4: ");
-            p4 = sc.nextDouble();
-
-            System.out.println("Digite S para sair ou M para mostrar o resultado: ");
-            x = sc.next();
-
-            if (x.equalsIgnoreCase("m")){
-                Double resultado = p1+p2+p3+p4;
-                System.out.print("Resultado: " + resultado);
-            }
+        do{
+            contador++;
+            System.out.print("Produto: " + contador + "\n");
+            System.out.print("Preço: ");
+            preco = sc.nextDouble();
+            total = total + preco; // Acumula no total (não no preço individual)
+            System.out.println("Preço inserido!");
+            System.out.println("Digite C para Continuar ou S para Sair!");
+            situacao = sc.next();
+            System.out.println("------***-----");
         }
-        while(x.equalsIgnoreCase("m"));
-        System.exit(0);
-
+        while(!situacao.equalsIgnoreCase("s"));
+            System.out.println("Total: " + total);
     }
 }
