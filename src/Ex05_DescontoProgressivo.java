655a4ed9da21da92b0aebca4611ccbec9d3d02b1
SamCodeX01 <txtstring@gmail.com>
2025-07-04 16:50:40 -0300

    Exercicio 5 criado

---
+++ src/Ex05_DescontoProgressivo.java
@@ -0,0 +1,16 @@
+public class Ex05_DescontoProgressivo {
+        /*5. Desconto progressivo
+        Para uma loja, aplique descontos progressivos: a cada 10 produtos comprados,
+        dê 10% de desconto no total. Use for para simular clientes comprando diferentes quantidades.
+        */
+    public static void main(String[] args) {
+
+
+        for(int i = 1; i < 10; i++){
+
+
+
+        }
+
+    }
+}
