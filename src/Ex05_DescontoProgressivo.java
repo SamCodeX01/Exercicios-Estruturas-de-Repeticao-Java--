ae5f031c3768e3dbd4621c875e30203048c1cb88
SamCodex01 <txtstring@gmail.com>
2025-07-11 23:05:36 -0300

    Exercicio 5, atualizado!

--- src/Ex05_DescontoProgressivo.java
+++ src/Ex05_DescontoProgressivo.java
@@ -9,21 +9,37 @@ public class Ex05_DescontoProgressivo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double preco, total = 0;
+        double total = 0;
+        double preco;
 
-        for (int i = 1; i < 20; i++) {
+        for (int i = 1; i < 4; i++) {
             System.out.println("Produto: " + i);
-            System.out.print("Digite o preço: ");
-            preco = sc.nextDouble();
+            System.out.print("Digite o preço ou `s` para sair: ");
+            //preco = sc.nextDouble();
 
-            if(i >= 10){
-                System.out.println("aplicar desconto");
-                //condição de saida é só usar o break
+            if (sc.hasNext("s")){//Verifica se a entrada é "s" antes de ler como double
+                sc.next(); //Consome o "s" do buffer
+                System.out.println("saindo...");
+                break;
             }
-
-            total = total + preco;
+            else if(sc.hasNextDouble()){// Se não for "s", tenta ler como double
+                preco = sc.nextDouble();
+                total += preco;
+            }
+            else{
+                System.out.println("Digite `s` ou um valor válido!");
+                sc.next(); // Limpa o buffer
+                i--; // Repete a iteração
+            }
         }
-        //System.out.print("Total: " + total);
+        System.out.println("Valor total: " + total);
+
+                    if(i >= 10 && i <21){
+                        System.out.println("desconto 10%");
+                        if(i == 0){
+                            break;
+                        }
+                    }
     }
 }
 
